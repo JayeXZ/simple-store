960112a20b0cb7b862735685ce660d782d204f88
Jaye Stanley P. Buhwilon <buhwilonjaye@gmail.com>
2026-03-23 17:54:02 +0800

    Enhance blog post design with improved card styling and hover effects

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Blog Posts</h1>
+    <h1 class="blog-title">Latest Blog Posts</h1>
 
     @foreach($posts as $post)
         <div class="card">
